90a286d1efed4cb8a781fcd5229c1552c89332e3
H. Boyd <harrisonboyd400@gmail.com>
2023-08-08 16:28:02 -0400

    Basic device discovery

--- Creative SignalRGB Bridge/Creative_SignalRGB_Bridge.cs
+++ Creative SignalRGB Bridge/Creative_SignalRGB_Bridge.cs
@@ -13,13 +13,51 @@ using System.ServiceProcess;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+using System.Text;
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
 
 namespace Creative_SignalRGB_Bridge
 {
     public partial class Creative_SignalRGB_Bridge : ServiceBase
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern SafeFileHandle CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode, IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, IntPtr hTemplateFile);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool DeviceIoControl(SafeFileHandle hDevice, uint dwIoControlCode, IntPtr lpInBuffer, uint nInBufferSize, IntPtr lpOutBuffer, uint nOutBufferSize, out uint lpBytesReturned, IntPtr lpOverlapped);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, IntPtr Enumerator, IntPtr hwndParent, uint Flags);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto)]
+        static extern bool SetupDiEnumDeviceInterfaces(IntPtr hDevInfo, IntPtr devInfo, ref Guid interfaceClassGuid, uint memberIndex, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData);
+
+        [DllImport("setupapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool SetupDiGetDeviceInterfaceDetail(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData, IntPtr deviceInterfaceDetailData, uint deviceInterfaceDetailDataSize, ref uint requiredSize, IntPtr deviceInfoData);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct SP_DEVICE_INTERFACE_DATA
+        {
+            public int cbSize;
+            public Guid interfaceClassGuid;
+            public int flags;
+            private IntPtr reserved;
+        }
+        const uint DIGCF_PRESENT = 0x02;
+        const uint DIGCF_DEVICEINTERFACE = 0x10;
+
+
         private const int ListenPort = 12345; 
-        private const string SearchMessage = "service.broadcast(\"Z-SEARCH * \\r\\n\")";
+        private const string SearchMessage = "Z-SEARCH *";
         private UdpClient listener = null;
 
         public Creative_SignalRGB_Bridge()
@@ -44,7 +82,40 @@ namespace Creative_SignalRGB_Bridge
 
         private bool discoverDevice()
         {
-            return true;
+            Guid deviceInterfaceClassGuid = new Guid("c37acb87-d563-4aa0-b761-996e7864af79");
+
+            IntPtr deviceInfoSet = SetupDiGetClassDevs(ref deviceInterfaceClassGuid, IntPtr.Zero, IntPtr.Zero, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+            if (deviceInfoSet != IntPtr.Zero)
+            {
+                SP_DEVICE_INTERFACE_DATA deviceInterfaceData = new SP_DEVICE_INTERFACE_DATA();
+                deviceInterfaceData.cbSize = Marshal.SizeOf(deviceInterfaceData);
+
+                if (SetupDiEnumDeviceInterfaces(deviceInfoSet, IntPtr.Zero, ref deviceInterfaceClassGuid, 0, ref deviceInterfaceData))
+                {
+                    uint requiredSize = 0;
+                    SetupDiGetDeviceInterfaceDetail(deviceInfoSet, ref deviceInterfaceData, IntPtr.Zero, 0, ref requiredSize, IntPtr.Zero);
+
+                    IntPtr detailDataBuffer = Marshal.AllocHGlobal((int)requiredSize);
+
+                    Marshal.WriteInt32(detailDataBuffer, (IntPtr.Size == 4) ? (4 + Marshal.SystemDefaultCharSize) : 8);
+
+                    if (SetupDiGetDeviceInterfaceDetail(deviceInfoSet, ref deviceInterfaceData, detailDataBuffer, requiredSize, ref requiredSize, IntPtr.Zero))
+                    {
+                        IntPtr pDevicePathName = new IntPtr(detailDataBuffer.ToInt64() + 4);
+                        string devicePath = Marshal.PtrToStringAuto(pDevicePathName);
+
+
+                        Console.WriteLine("Device path: " + devicePath);
+                        Console.ReadKey();
+                        return true;
+                        //openDevice(devicePath);
+                    }
+            
+
+                    Marshal.FreeHGlobal(detailDataBuffer);
+                }
+            }
+            return false;
         }
 
         private async void StartListening()
@@ -72,10 +143,11 @@ namespace Creative_SignalRGB_Bridge
         {
             string message = Encoding.UTF8.GetString(result.Buffer);
 
-            System.Diagnostics.Debug.WriteLine(message);
+            Debug.WriteLine(message);
 
             if (message.Trim().Equals(SearchMessage))
             {
+                Debug.WriteLine("Recieved Search Message");
                 string responseMessage = "Your response here";
                 byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
                 listener.Send(responseData, responseData.Length, result.RemoteEndPoint);
