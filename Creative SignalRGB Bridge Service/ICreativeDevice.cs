11ec16a695cddabea51861f2e65570bdc804bf79
H. Boyd <harrisonboyd400@gmail.com>
2023-08-29 16:29:32 -0500

    Add interface for devices

---
+++ Creative SignalRGB Bridge Service/ICreativeDevice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Creative_SignalRGB_Bridge_Service
+{
+    public interface ICreativeDevice
+    {
+        string DevicePath
+        {
+            get;
+        }
+
+        string DeviceName
+        {
+        get;
+
+        }
+
+        Task<bool> DiscoverDeviceAsync();
+
+        Task<bool> SendCommand(byte[] command);
+
+        Task<bool> SetColors(byte[][] colors);
+
+
+    }
+}
