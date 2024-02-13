33787165c6520a6ce271a884240babd0aecb86dc
H. Boyd <8950185+hboyd2003@users.noreply.github.com>
2024-02-13 15:12:03 -0600

    Update License Headers

--- Creative SignalRGB Bridge Service/CreativeSignalRGBBridgeService.cs
+++ Creative SignalRGB Bridge Service/CreativeSignalRGBBridgeService.cs
@@ -1,5 +1,5 @@
 ﻿// This is the Creative SignalRGB Bridge Plugin/Service.
-// Copyright © 2023 Harrison Boyd
+// Copyright © 2023-2024 Harrison Boyd
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
